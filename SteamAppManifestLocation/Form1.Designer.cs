bb08e1b4ea834e339e5bb8f258d57b23f63367c1
max_301 <wqr3011@gmail.com>
2016-07-02 12:26:36 +0800

    添加项目文件。

---
+++ SteamAppManifestLocation/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace SteamAppManifestLocation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SteamAppLocate = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGN = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_Result = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SteamApp文件夹位置";
+            // 
+            // SteamAppLocate
+            // 
+            this.SteamAppLocate.AutoSize = true;
+            this.SteamAppLocate.Location = new System.Drawing.Point(12, 32);
+            this.SteamAppLocate.Name = "SteamAppLocate";
+            this.SteamAppLocate.Size = new System.Drawing.Size(113, 12);
+            this.SteamAppLocate.TabIndex = 1;
+            this.SteamAppLocate.Text = "showSteamAppLocate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "游戏名";
+            // 
+            // txtGN
+            // 
+            this.txtGN.Location = new System.Drawing.Point(59, 56);
+            this.txtGN.Name = "txtGN";
+            this.txtGN.Size = new System.Drawing.Size(274, 21);
+            this.txtGN.TabIndex = 3;
+            this.txtGN.Tag = "";
+            this.txtGN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtGN_KeyPress);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(339, 54);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "搜索";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "状态";
+            // 
+            // lbl_Result
+            // 
+            this.lbl_Result.Location = new System.Drawing.Point(14, 119);
+            this.lbl_Result.Name = "lbl_Result";
+            this.lbl_Result.ReadOnly = true;
+            this.lbl_Result.Size = new System.Drawing.Size(319, 21);
+            this.lbl_Result.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(233, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "本软件帮助您定位Steam的appmanifest文件";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "开发者：天空";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(205, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(209, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "以帮助您向好友复制已下载的游戏文件";
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(339, 117);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(75, 23);
+            this.btn_select.TabIndex = 10;
+            this.btn_select.Text = "打开位置";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(426, 218);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_Result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtGN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SteamAppLocate);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Steam Appmanifest 定位工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label SteamAppLocate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGN;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox lbl_Result;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_select;
+    }
+}
+
---
+++ SteamAppManifestLocation/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteamAppManifestLocation
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        bool isdone=false;
+        public Form1()
+        {
+            FolderBrowserDialog dilog = new FolderBrowserDialog();
+            dilog.Description = "请选择SteamApp目录";
+            DialogResult dr = dilog.ShowDialog();
+            if ( dr == DialogResult.OK || dr == DialogResult.Yes)
+            {
+                path = dilog.SelectedPath;
+                path = path + "\\";
+            }
+            else
+            {
+                MessageBox.Show("你没有选择SteamApp目录", "选择SteamApp目录");
+                System.Environment.Exit(0);
+            }
+            bool isStreamApp = File.Exists(path + "libraryfolders.vdf");
+            if (!isStreamApp)
+            {
+                MessageBox.Show("SteamApp目录无效", "目录无效");
+                System.Environment.Exit(0);
+            }
+            InitializeComponent();
+            SteamAppLocate.Text = path;
+            SetWindowPos(this.Handle, -1, 0, 0, 0, 0, 1 | 2);
+            SetWindowPos(this.Handle, -2, 0, 0, 0, 0, 1 | 2);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            isdone = false;
+            DirectoryInfo dirinfo = new DirectoryInfo(path);
+            bool found = false;
+            foreach (FileInfo f in dirinfo.GetFiles("*.acf"))
+            {
+                string[] acf=File.ReadAllLines(path + f.ToString());
+                foreach (string line in acf)
+                {
+                    string linel=line.Trim();
+                    string[] spl = linel.Replace("\t\t","\t").Split('\t');
+                    if (spl.Length > 1)
+                    {
+                        if (spl[0].Equals("\"name\""))
+                        {
+                            string tr = spl[1].Replace('\"', ' ').Trim();
+                            if (tr.Equals(txtGN.Text.ToString(),StringComparison.OrdinalIgnoreCase))
+                            {
+                                lbl_Result.Text = path + f.ToString();
+                                found = true;
+                                isdone = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (found) break;
+            }
+            if (!found) lbl_Result.Text = "没有找到对应的appmanifest文件，请确认游戏名正确。";
+        }
+
+        private void txtGN_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if(e.KeyChar != (char)13)
+            {
+                return;
+            }
+            else
+            {
+                btnSearch_Click(sender, null);
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int Width, int Height, int flags);
+        /// <summary>
+        /// 得到当前活动的窗口
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern System.IntPtr GetForegroundWindow();
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            if (isdone)
+            {
+                System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo("Explorer.exe");
+                info.Arguments = "/n,/select," + lbl_Result.Text;
+                System.Diagnostics.Process.Start(info);
+            }
+            else
+            {
+                MessageBox.Show("请搜索文件后再试！","无法获取");
+            }
+        }
+    }
+}
